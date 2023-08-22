292c4d3e9453c9494830f6bb9945ad45121768f9
xarhang <xarhang@STB.LA>
2023-08-22 17:11:30 +0700

    check and change database

--- db/conn.go
+++ db/conn.go
@@ -7,6 +7,7 @@ import (
 	"pos-go/model"
 
 	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
@@ -16,12 +17,23 @@ var Conn *gorm.DB
 
 func ConnectDB() {
 	dsn := os.Getenv("DATABASE_DSN")
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
+	//check if Production or not
+	if env := os.Getenv("APP_ENV"); env == "production" {
+		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
 
-	if err != nil {
-		log.Fatal("Cannot Connect to the database " + err.Error())
+		if err != nil {
+			log.Fatal("Cannot Connect to the database " + err.Error())
+		}
+		Conn = db
+	} else {
+		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
+
+		if err != nil {
+			log.Fatal("Cannot Connect to the database mysql " + err.Error())
+		}
+		Conn = db
 	}
-	Conn = db
+
 }
 
 func Migrate() {
